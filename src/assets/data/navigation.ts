61592ec9a81cf9acc4ff728bce8e9213d12e4fa3
Diana Butiakova <butiakova.diana@gmail.com>
2025-06-06 16:27:44 +0600

    feat: add navigation items data

---
+++ src/assets/data/navigation.ts
@@ -0,0 +1,14 @@
+import type { navItem } from '../../types';
+
+const navItems: navItem[] = [
+  {
+    key: '/articles/about',
+    title: 'О нас',
+  },
+  {
+    key: '/articles/ceremony',
+    title: 'Церемония',
+  },
+];
+
+export default navItems;
---
+++ src/types.ts
@@ -0,0 +1,14 @@
+import type { TabIcon } from 'antd-mobile-v2/lib/tab-bar/PropsType';
+
+export type localeString = {
+  ru: string;
+  kg?: string;
+  en?: string;
+};
+
+export type navItem = {
+  key: string;
+  title: string;
+  icon?: TabIcon;
+  menu?: boolean;
+};
