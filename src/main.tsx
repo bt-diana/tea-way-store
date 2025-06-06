076c7d0bee5c70c26ed22e5521543798c11d7b85
Diana Butiakova <butiakova.diana@gmail.com>
2025-06-06 16:26:35 +0600

    feat: add andt localization

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './index.css';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ConfigProvider } from 'antd';
+import ruRU from 'antd/locale/ru_RU';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ConfigProvider locale={ruRU}>
+      <App />
+    </ConfigProvider>
+  </StrictMode>
+);
