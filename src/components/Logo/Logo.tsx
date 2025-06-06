02805b9a65fe604f94ddf911ba496c112f7564e8
Diana Butiakova <butiakova.diana@gmail.com>
2025-06-06 16:28:06 +0600

    feat: add Logo component

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,12 @@
+import './Logo.css';
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <img src="/TeaWayVector.svg" className="logo-img" />
+      <div className="logo-title">Tea Way</div>
+    </div>
+  );
+};
+
+export default Logo;
