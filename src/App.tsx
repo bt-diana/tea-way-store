9160c32a4914e93f8a38ce02ba1a5dc8deaf3ca8
Diana Butiakova <butiakova.diana@gmail.com>
2025-06-06 16:28:54 +0600

    feat: put Navigation component into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import './App.css'
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+const App = () => {
+  return <Navigation />;
+};
 
-function App() {
-  return (
-    <div>Tea Way</div>
-  )
-}
-
-export default App
+export default App;
