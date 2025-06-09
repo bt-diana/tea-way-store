94594e70770923154adea80bcc285e1619c191f5
Diana Butiakova <butiakova.diana@gmail.com>
2025-06-09 20:26:27 +0600

    refactor: reuse Navigation component for bottom navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import './App.css';
-import BottomNavigation from './components/BottomNavigation/BottomNavigation';
 import Navigation from './components/Navigation/Navigation';
 import Content from './components/Content/Content';
 
 const App = () => {
   return (
     <>
-      <Navigation />
+      <Navigation mobile={true} />
       <Content />
-      <BottomNavigation />
+      <Navigation mobile={true} bottom={true} />
     </>
   );
 };
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -3,16 +3,38 @@ import { TabBar } from 'antd-mobile-v2';
 import { navItems } from '../../assets/data';
 import Logo from '../Logo/Logo';
 
-const Navigation = () => {
+type NavigationProps = {
+  mobile?: boolean;
+  bottom?: boolean;
+};
+
+const Navigation = ({ mobile, bottom }: NavigationProps) => {
+  if (mobile) {
+    return (
+      <nav className={bottom ? "bottom-navigation" : "navigation"}>
+        {!bottom ? <Logo /> : null}
+        <TabBar>
+          {navItems
+            .filter(({ menu }) => menu == bottom)
+            .map(({ key, title, icon }) => (
+              <TabBar.Item key={key} title={title} icon={icon} />
+            ))}
+        </TabBar>
+      </nav>
+    );
+  }
+
+  if (bottom) {
+    return null;
+  }
+
   return (
     <nav className="navigation">
       <Logo />
       <TabBar>
-        {navItems
-          .filter(({ menu }) => !menu)
-          .map(({ key, title }) => (
-            <TabBar.Item key={key} title={title} />
-          ))}
+        {navItems.map(({ key, title }) => (
+          <TabBar.Item key={key} title={title} />
+        ))}
       </TabBar>
     </nav>
   );
