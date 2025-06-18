d29dad4e83a6e50ed2a11462e1aeb128557275c2
Diana Butiakova <butiakova.diana@gmail.com>
2025-06-18 20:03:47 +0600

    feat: move navItems directly to Navagation component and add PopUpMenu

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,8 +1,17 @@
 import { TabBar } from 'antd-mobile-v2';
-import { navItems } from '../assets/data';
 import Logo from './Logo';
 import { useNavigate } from 'react-router-dom';
+import PopUpMenu from './PopUpMenu';
 import { useState } from 'react';
+import {
+  CloseOutlined,
+  GlobalOutlined,
+  HomeOutlined,
+  MenuOutlined,
+  SearchOutlined,
+  ShoppingCartOutlined,
+} from '@ant-design/icons';
+import type { navItem } from '../types/navItems';
 
 const { Item } = TabBar;
 
@@ -14,23 +23,90 @@ const Navigation = ({ bottom }: NavigationProps) => {
   const navigate = useNavigate();
   const [menuIsVisible, setMenuIsVisible] = useState(false);
 
+  const navItems: navItem[] = [
+    {
+      url: '/articles/about',
+      key: 'about',
+      title: 'О нас',
+      onPress() {
+        navigate(this.url!);
+      },
+    },
+    {
+      url: '/articles/ceremony',
+      key: 'ceremony',
+      title: 'Церемония',
+      onPress() {
+        navigate(this.url!);
+      },
+    },
+    {
+      url: '/',
+      key: 'main',
+      title: 'Главная',
+      icon: <HomeOutlined />,
+      menu: true,
+      onPress() {
+        navigate(this.url!);
+      },
+    },
+    {
+      key: 'search',
+      title: 'Поиск',
+      icon: <SearchOutlined />,
+      menu: true,
+      onPress: () => {
+        setMenuIsVisible((isVisible) => !isVisible);
+      },
+    },
+    {
+      key: 'catalog',
+      title: 'Каталог',
+      icon: <MenuOutlined />,
+      activeIcon: <CloseOutlined />,
+      menu: true,
+      onPress() {
+        setMenuIsVisible((isVisible) => !isVisible);
+      },
+    },
+    {
+      url: '/cart',
+      key: 'cart',
+      icon: <ShoppingCartOutlined />,
+      title: 'Корзина',
+      menu: true,
+      onPress() {
+        navigate(this.url!);
+      },
+    },
+    {
+      key: 'lang',
+      title: 'Язык',
+      icon: <GlobalOutlined />,
+      menu: true,
+      onPress() {},
+    },
+  ];
+
   return (
     <nav className={bottom ? 'bottom-navigation' : 'navigation'}>
       {!bottom ? <Logo /> : null}
       <TabBar>
         {navItems
           .filter(({ menu }) => menu == bottom)
-          .map(({ key, title, icon, url, alterIcon }) => (
+          .map(({ key, title, icon, activeIcon, onPress }) => (
             <Item
               key={key}
               title={title}
-              icon={alterIcon && menuIsVisible ? alterIcon : icon}
-              onPress={() =>
-                url ? navigate(url) : setMenuIsVisible((value) => !value)
-              }
+              icon={activeIcon ? (menuIsVisible ? activeIcon : icon) : icon}
+              onPress={onPress}
             />
           ))}
       </TabBar>
+      <PopUpMenu
+        visible={menuIsVisible}
+        close={() => setMenuIsVisible(false)}
+      />
     </nav>
   );
 };
