13868ac044999372e018c7e2c08b4482ea934bd9
Diana Butiakova <butiakova.diana@gmail.com>
2025-07-03 14:12:26 +0600

    feat: add first MaingPage section

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,26 @@
+import Section from '../components/Section';
+import type { SectionData } from '../types/sectionData';
+import { Title } from '../types/titile';
+
+const sections: SectionData[] = [
+  {
+    title: 'Чайный Путь — настоящий вкус чая!',
+    TitleLevel: Title.h1,
+    paragraph:
+      'Откройте мир настоящего китайского чая. Только отборные сорта, прямиком с лучших чайных плантаций Китая.',
+  },
+];
+
 const MainPage = () => {
-  return <h1>MainPage</h1>;
+  return (
+    <div className="page">
+      {sections.map(({ title, TitleLevel, paragraph, children }) => (
+        <Section title={title} TitleLevel={TitleLevel} paragraph={paragraph}>
+          {children}
+        </Section>
+      ))}
+    </div>
+  );
 };
 
 export default MainPage;
