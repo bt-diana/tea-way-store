87fa3c3d8da7e78a00fc7bb487e04da939bffe98
Diana Butiakova <butiakova.diana@gmail.com>
2025-07-03 13:29:09 +0600

    chore: add antd compatibility package

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client';
 import { ConfigProvider } from 'antd';
 import ruRU from 'antd/locale/ru_RU';
 import App from './App.tsx';
+import '@ant-design/v5-patch-for-react-19';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
