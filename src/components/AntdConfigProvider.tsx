9c7500cc3eb10fe99b49cf0126d867a843d26220
Diana Butiakova <butiakova.diana@gmail.com>
2025-07-06 15:36:40 +0600

    feat: remove Contact component and use antd Button component instead

--- src/components/AntdConfigProvider.tsx
+++ src/components/AntdConfigProvider.tsx
@@ -5,8 +5,19 @@ type AntdConfigProvider = {
   children: ReactNode;
 };
 
+const theme = {
+  token: {
+    colorBgLayout: 'rgba(255, 251, 244, 1)',
+    colorPrimary: 'rgba(65, 117, 86, 1)',
+    colorInfo: 'rgba(65, 117, 86, 1)',
+    colorLink: 'rgba(108, 108, 108, 1)',
+    colorTextBase: 'rgba(34, 34, 34, 1)',
+    fontFamily: 'Roboto',
+  },
+};
+
 const AntdConfigProvider = ({ children }: AntdConfigProvider) => (
-  <ConfigProvider theme={{}}>{children}</ConfigProvider>
+  <ConfigProvider theme={theme}>{children}</ConfigProvider>
 );
 
 export default AntdConfigProvider;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,7 @@ import type { ContactsItem } from '../types/contactsItem';
 import type { SectionData } from '../types/sectionData';
 import { Title } from '../types/titile';
 import Section from './Section';
-import Contact from './Contact';
+import { Button } from 'antd';
 
 const data: SectionData = {
   title: 'Свяжитесь с нами',
@@ -40,33 +40,39 @@ const contacts: ContactsItem[] = [
 
 const socials: ContactsItem[] = [
   {
-    icon: <FacebookOutlined />,
+    icon: <FacebookOutlined style={{ fontSize: '36px' }} />,
   },
   {
-    icon: <InstagramOutlined />,
+    icon: <InstagramOutlined style={{ fontSize: '36px' }} />,
   },
   {
-    icon: <YoutubeOutlined />,
+    icon: <YoutubeOutlined style={{ fontSize: '36px' }} />,
   },
   {
-    icon: <WhatsAppOutlined />,
+    icon: <WhatsAppOutlined style={{ fontSize: '36px' }} />,
   },
 ];
 
 const Footer = () => {
   return (
     <footer>
-      <Section title={data.title} className="contacts-section">
+      <Section
+        title={data.title}
+        TitleLevel={Title.h1}
+        className="contacts-section"
+      >
         <div className="contacts">
           {contacts.map(({ icon, text, link }) => (
-            <Contact icon={icon} text={text} link={link} />
+            <Button type="link" icon={icon} href={link}>
+              {text}
+            </Button>
           ))}
         </div>
       </Section>
       <Section className="socials-section">
         <div className="socials">
-          {socials.map(({ icon, text, link }) => (
-            <Contact icon={icon} text={text} link={link} />
+          {socials.map(({ icon, link }) => (
+            <Button variant="link" color="primary" icon={icon} href={link} />
           ))}
         </div>
       </Section>
