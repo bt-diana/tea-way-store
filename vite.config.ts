496323ceb71df54c6865b1dc1790b1dcbd900e1c
Diana Butiakova <butiakova.diana@gmail.com>
2025-07-08 18:15:18 +0600

    feat: execute env variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+dotenv.config();
+
+export default defineConfig((props) => {
+  const env = loadEnv(props.mode, process.cwd(), 'VITE');
+  const envWithProcessPrefix = {
+    'process.env': `${JSON.stringify(env)}`,
+  };
+
+  return {
+    plugins: [react()],
+    define: envWithProcessPrefix,
+    base: './',
+  };
 });
