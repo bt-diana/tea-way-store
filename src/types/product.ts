2b4a8990aab267981ecfd9990daf33ffa1e77b35
Diana Butiakova <butiakova.diana@gmail.com>
2025-07-22 15:36:22 +0600

    feat: change product properties

--- src/types/product.ts
+++ src/types/product.ts
@@ -4,8 +4,8 @@ export type ProductRaw = {
   id: string;
   name: string;
   typeId: string;
-  cover: string;
-  description: string;
+  cover?: string;
+  description?: string;
   year?: number;
   regionId?: string;
   tastesId?: string[];
@@ -20,8 +20,8 @@ export type Product = {
   id: string;
   name: string;
   type: string;
-  cover: string;
-  description: string;
+  cover?: string;
+  description?: string;
   sizesPrices: ProductSizePrice[];
   year?: number;
   region?: string;
