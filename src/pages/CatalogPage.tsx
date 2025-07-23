7b133b7b0f89dd844e19bfb967f9bf02f467c011
Diana Butiakova <butiakova.diana@gmail.com>
2025-07-23 16:22:55 +0600

    feat: create tags in catalogPage

--- src/pages/CatalogPage.tsx
+++ src/pages/CatalogPage.tsx
@@ -1,20 +1,30 @@
-import { useParams } from 'react-router-dom';
+import { Link, useLocation, useParams } from 'react-router-dom';
 import Section from '../components/Section';
 import { useEffect, useState } from 'react';
 import ProductsList from '../components/ProductsList';
-import { getTypeById } from '../api/productTypes';
+import { getChildrenTypesByParentId, getTypeById } from '../api/productTypes';
 import { Title } from '../types/titile';
 import type { ProductType } from '../types/productType';
 import ResourceNotFoundPage from './ResourceNotFoundPage';
+import { Tag } from 'antd';
 
 const CatalogPage = () => {
+  const { pathname } = useLocation();
   const { id } = useParams();
   const [type, setType] = useState<ProductType | undefined>();
+  const [childrenTypes, setChildrenTypes] = useState<
+    ProductType[] | undefined
+  >();
 
   useEffect(() => {
     if (id) {
       getTypeById(id).then((type) => {
         setType(type);
+        if (type) {
+          getChildrenTypesByParentId(type.id).then((types) => {
+            setChildrenTypes(types);
+          });
+        }
       });
     }
   }, [id, setType]);
@@ -25,7 +35,22 @@ const CatalogPage = () => {
 
   return (
     <main>
-      <Section title={type.name} TitleLevel={Title.h1}>
+      <Section
+        title={type.name}
+        TitleLevel={Title.h1}
+        paragraph={type.description}
+      >
+        <div className="catalog-tags">
+          {childrenTypes?.map(({ id, name }) => (
+            <Link to={`${pathname}/${id}`}>
+              <Tag key={id} className="catalog-tag">
+                {name}
+              </Tag>
+            </Link>
+          ))}
+        </div>
+      </Section>
+      <Section>
         <ProductsList typeId={type.id} />
       </Section>
     </main>
