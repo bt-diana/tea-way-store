c9207098fb099fa590c38903514b28142aa317f7
Diana Butiakova <butiakova.diana@gmail.com>
2025-07-23 14:20:26 +0600

    feat: create ResourseNotFoundPage

---
+++ src/pages/ResourseNotFoundPage.tsx
@@ -0,0 +1,11 @@
+import { Empty } from 'antd';
+
+const ResourseNotFoundPage = () => {
+  return (
+    <main style={{ height: '100%' }}>
+      <Empty style={{ margin: 'auto' }} />
+    </main>
+  );
+};
+
+export default ResourseNotFoundPage;
