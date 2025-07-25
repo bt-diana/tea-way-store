e9a8056a2fa62205226fd3be1567232cdaa6d379
Diana Butiakova <butiakova.diana@gmail.com>
2025-07-25 17:38:12 +0600

    feat: get contacts from .env

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,6 +14,15 @@ import { Title } from '../types/titile';
 import Section from './Section';
 import { Button } from 'antd';
 
+const CONTACTS_PHONE = process.env.VITE_CONTACTS_PHONE;
+const CONTACTS_EMAIL = process.env.VITE_CONTACTS_EMAIL;
+const CONTACTS_INST = process.env.VITE_CONTACTS_INST;
+const CONTACTS_WHATSAPP = process.env.VITE_CONTACTS_WHATSAPP;
+const CONTACTS_FACEBOOK = process.env.VITE_CONTACTS_FACEBOOK;
+const CONTACTS_YOUTUBE = process.env.VITE_CONTACTS_YOUTUBE;
+const CONTACTS_ADDRESS = process.env.VITE_CONTACTS_ADDRESS;
+const CONTACTS_BUSINESS_HOURS = process.env.VITE_CONTACTS_BUSINESS_HOURS;
+
 const data: SectionData = {
   title: 'Свяжитесь с нами',
 };
@@ -21,35 +30,40 @@ const data: SectionData = {
 const contacts: Contact[] = [
   {
     icon: <PhoneOutlined />,
-    text: '+996-000-000-000',
-    link: 'tel:+996-000-000-000',
+    text: CONTACTS_PHONE,
+    link: `tel:${CONTACTS_PHONE}`,
   },
   {
     icon: <MailOutlined />,
-    text: 'teaway@email.com',
+    text: CONTACTS_EMAIL,
+    link: `mailto:${CONTACTS_EMAIL}`,
   },
   {
     icon: <EnvironmentOutlined />,
-    text: 'Бишкек, микрорайон Асанбай, 78/1',
+    text: CONTACTS_ADDRESS,
   },
   {
     icon: <ClockCircleOutlined />,
-    text: 'Ежедневно 09:00–21:00',
+    text: CONTACTS_BUSINESS_HOURS,
   },
 ];
 
 const socials: Contact[] = [
   {
-    icon: <FacebookOutlined style={{ fontSize: '36px' }} />,
+    icon: <WhatsAppOutlined style={{ fontSize: '36px' }} />,
+    link: CONTACTS_WHATSAPP,
   },
   {
     icon: <InstagramOutlined style={{ fontSize: '36px' }} />,
+    link: CONTACTS_INST,
   },
   {
     icon: <YoutubeOutlined style={{ fontSize: '36px' }} />,
+    link: CONTACTS_YOUTUBE,
   },
   {
-    icon: <WhatsAppOutlined style={{ fontSize: '36px' }} />,
+    icon: <FacebookOutlined style={{ fontSize: '36px' }} />,
+    link: CONTACTS_FACEBOOK,
   },
 ];
 
@@ -62,24 +76,32 @@ const Footer = () => {
         className="contacts-section"
       >
         <div className="contacts">
-          {contacts.map(({ icon, text, link }, index) => (
-            <Button key={index} type="link" icon={icon} href={link}>
-              {text}
-            </Button>
-          ))}
+          {contacts.map(({ icon, text, link }, index) => {
+            if (text) {
+              return (
+                <Button key={index} type="link" icon={icon} href={link}>
+                  {text}
+                </Button>
+              );
+            }
+          })}
         </div>
       </Section>
       <Section className="socials-section">
         <div className="socials">
-          {socials.map(({ icon, link }, index) => (
-            <Button
-              key={index}
-              variant="link"
-              color="primary"
-              icon={icon}
-              href={link}
-            />
-          ))}
+          {socials.map(({ icon, link }, index) => {
+            if (link) {
+              return (
+                <Button
+                  key={index}
+                  variant="link"
+                  color="primary"
+                  icon={icon}
+                  href={link}
+                />
+              );
+            }
+          })}
         </div>
       </Section>
     </footer>
