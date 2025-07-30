a1404d53b31dc04e3787e96bdd3b8c87e2fba7a2
Diana Butiakova <butiakova.diana@gmail.com>
2025-07-30 19:18:46 +0600

    feat: use whatsapp link from env in ContactFloatButton

--- src/components/ContactFloatButton.tsx
+++ src/components/ContactFloatButton.tsx
@@ -1,10 +1,15 @@
 import { MessageOutlined } from '@ant-design/icons';
 import { FloatButton } from 'antd';
 
+const CONTACTS_WHATSAPP = process.env.VITE_CONTACTS_WHATSAPP;
+
 const ContactFloatButton = () => {
-  const link = '';
   return (
-    <a href={link} target="_blank" className="contact-float-button">
+    <a
+      href={CONTACTS_WHATSAPP}
+      target="_blank"
+      className="contact-float-button"
+    >
       <FloatButton icon={<MessageOutlined />} />
     </a>
   );
